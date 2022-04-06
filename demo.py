5927b4f3fb4aac92ab8a40c31578229c7406d9d5
Wang Xiaoke <15968029156@163.com>
2022-04-06 16:26:08 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,17 @@
+"""
+练习pycharm
+作者：wxk
+日期：2022年04月04日
+"""
+import sys
+import requests
+
+# print('hello world')
+# 命令行编程
+# print(sys.argv)
+# print(sys.argv[1])
+
+# 三方库的引入
+response = requests.get("https://www.baidu.com")
+print(response.text)
+
